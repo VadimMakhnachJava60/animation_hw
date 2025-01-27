2fdc0145b0e406508408e2818bdb91ce45b4bc14
Vadimmakhnach <asutokir@gmail.com>
2025-01-27 19:55:23 +0200

    animation_hw

---
+++ src/animation.js
@@ -0,0 +1,7 @@
+let boxAnimationElement = document.getElementById("moving-circle");
+function start() {
+  boxAnimationElement.classList.add("animat");
+}
+function stop() {
+  boxAnimationElement.classList.remove("animat");
+}
